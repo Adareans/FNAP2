e03a9e10a83197920aba8a635dd7128c5cda11e0
Adrian Vargas <AVargas6017@student.pps.net>
2022-04-26 12:44:32 -0700

    door

---
+++ FNAP2/Assets/Scripts/DoorClose.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorClose : MonoBehaviour
+{
+public GameObject Door;
+    public bool up;
+    public bool down;
+    public bool upstop;
+    public bool downstop;
+    public GameObject txtToDisplay;
+    private bool PlayerInZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        down = true;
+        downstop = true;
+        PlayerInZone = false;      
+        txtToDisplay.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (up == true && down == false && upstop == false) {
+            Door.transform.Translate(Vector3.up * Time.deltaTime * 15);
+        }
+        if (down == true && up == false && downstop == false) {
+            Door.transform.Translate(Vector3.down * Time.deltaTime * 15);
+        }
+
+        if(Input.GetKeyDown(KeyCode.E) && PlayerInZone) {
+            gameObject.GetComponent<AudioSource>().Play();
+            if (up && upstop) {
+                down = true;
+                up = false;
+                upstop = false;
+            } else if (down && downstop) {
+                up = true;
+                down = false;
+                downstop = false;
+            }
+        }
+        if (Door.transform.position.y > 10.5f)
+        {
+            upstop = true;
+            downstop = false;
+        }
+        if (Door.transform.position.y < 3f) {
+            downstop = true;
+            upstop = false;
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            txtToDisplay.SetActive(true);
+            PlayerInZone = true;
+        }
+     }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerInZone = false;
+            txtToDisplay.SetActive(false);
+        }
+    }
+
+
+}
